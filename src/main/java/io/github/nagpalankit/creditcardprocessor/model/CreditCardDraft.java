30ae43feebcc27d982f10b7e92789353b1d12f05
Ankit Nagpal <ank.nagpal@gmail.com>
2025-04-25 22:22:47 +0100

    Github Copilot suggested changes to change the data class to a record

--- src/main/java/io/github/nagpalankit/creditcardprocessor/model/CreditCardDraft.java
+++ src/main/java/io/github/nagpalankit/creditcardprocessor/model/CreditCardDraft.java
@@ -3,25 +3,16 @@ package io.github.nagpalankit.creditcardprocessor.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import lombok.Getter;
 
-@Data
-@Getter
-public class CreditCardDraft {
-    @NotBlank(message = "User name is mandatory.")
-    private final String userName;
+public record CreditCardDraft(
+        @NotBlank(message = "User name is mandatory.")
+        String userName,
 
-    @NotBlank(message = "Card number is mandatory.")
-    @Size(min = 16, max = 19, message = "Card number value must be between 16 to 19 digits.")
-    private final String cardNumber;
+        @NotBlank(message = "Card number is mandatory.")
+        @Size(min = 16, max = 19, message = "Card number value must be between 16 to 19 digits.")
+        String cardNumber,
 
-    @NotNull(message = "Card limit is mandatory.")
-    private final double cardLimit;
-
-    public CreditCardDraft(String userName, String cardNumber, double cardLimit) {
-        this.userName = userName;
-        this.cardNumber = cardNumber;
-        this.cardLimit = cardLimit;
-    }
+        @NotNull(message = "Card limit is mandatory.")
+        double cardLimit
+) {
 }

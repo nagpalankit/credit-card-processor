d8f24349404de3a4712b17cb1b00b5ccdb3c9fca
Ankit Nagpal <ank.nagpal@gmail.com>
2025-04-25 15:23:21 +0100

    Initialized repository

---
+++ src/main/java/io/github/nagpalankit/creditcardprocessor/CreditCardProcessorApplication.java
@@ -0,0 +1,12 @@
+package io.github.nagpalankit.creditcardprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardProcessorApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/nagpalankit/creditcardprocessor/CreditCardProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nagpalankit.creditcardprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

28df27f5b4db52a9c96b757fb30f7a455ee48569
Ankit Nagpal <ank.nagpal@gmail.com>
2025-04-27 10:28:47 +0100

    Added allowed origins header for front-end to access the APIs

--- src/main/java/io/github/nagpalankit/creditcardprocessor/CreditCardProcessorApplication.java
+++ src/main/java/io/github/nagpalankit/creditcardprocessor/CreditCardProcessorApplication.java
@@ -2,11 +2,24 @@ package io.github.nagpalankit.creditcardprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CreditCardProcessorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CreditCardProcessorApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/cards").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
